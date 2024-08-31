59fea0c07dc9c0780f7049fdacff19f6d8235be1
Sheri <esheri_slow@outlook.com>
2024-08-31 15:37:05 -0500

    added browser chat interface to chat
    Added browser chat interface to chat program with sidebar.

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
-app.py
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+
+    st.header("Chat with mulitple PDFs :books:")
+    st.text_input("Ask a question about your documents:")
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on 'Process'")
+        st.button("Process")
+
+    
+
+
+if __name__ == '__main__':
+    main()
